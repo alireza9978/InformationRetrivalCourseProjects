42d80a51dc2cbbb85fd1f75e579d49271ab4b87b
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-10-11 11:22:56 +0330

    fix bug

--- src/ExerciseOne/main.py
+++ src/ExerciseOne/main.py
@@ -18,22 +18,26 @@ if __name__ == '__main__':
     sample = "بنابرایـن ایـن مسـئله فرصـت خوبـی بـرای اسـتفاده از ایـن فضـای پویـا جهـت رونق بخشــیدن بــه کســب " \
              "وکارتان اســت ، به ویــژه ازاین جهــت کــه ایــن شــبکه امــکان تبلیغــات را از طریــق پســتها و " \
              "اســتوریها بــرای مــردم بســیار راحتتــر نمــوده اســت. عــلاوه بــر ایــن افــراد جوانتــر همیشــه " \
-             "علاقــه ی بیشــتری بــه شــبکه های اجتماعـی ماننـد اینسـتاگرام، یوتیـوب، توییتـر، فیـس بـوک و ایـن دسـت " \
+             "علاقــه ی بیشــتری بــه شــبکه های اجتماعـی ماننـد " \
+             "اینسـتاگرام، یوتیـوب، توییتـر، فیـس بـوک و ایـن دسـت " \
              "برنامه هـا داشـته اند و ایـن گـروه سـنی همیشـه خریـد اینترنتـی را بـه خریـد حضـوری ترجیـح میدهنـد، " \
              "زیـرا عـاوه بـر راحتـی ایـن نـوع از خریـد ، وقتـی ایـن دسـته از کاربـران از طریــق فروشــگاه های " \
-             "اینترنتــی معتبــر خریــد خــود را انجــام میدهنــد به نوعــی از اصالــت کالا ی خــود اطمینــان دارنــد " \
+             "اینترنتــی معتبــر خریــد خــود را انجــام میدهنــد به " \
+             "نوعــی از اصالــت کالا ی خــود اطمینــان دارنــد " \
              "و میداننــد کــه کالا یــی کــه قــرار اســت بــه دستشـان برسـد از کیفیـت بالا یـی برخـوردار اسـت . "
     sample = clean(sample)
     sample = sentences(sample)
-    tagger = POSTagger(model=r"")
+    tagger = POSTagger(model=r"/home/alireza/projects/python/InformationRetrivalCourseProjects/res/postagger.model")
     tokens_list = []
     tags_list = []
     for sen in sample:
-        tokens_list.append(word_tokenize(sen))
-        tags_list.extend(tagger.tag(sen))
+        tokens = word_tokenize(sen)
+        tokens_list.append(tokens)
+        tags_list.extend(tagger.tag(tokens))
 
     lemmatizer = Lemmatizer()
     lemmatized_words = []
     for tag in tags_list:
         lemmatized_words.append(lemmatizer.lemmatize(tag[0], pos=tag[1]))
-
+    print(lemmatized_words)
+    print(tags_list)
